22603d4524a9aab73a3d57017fefc8965e5cb79c
Ali <s202281300@kfupm.edu.sa>
2026-04-06 08:44:11 +0300

    Fixed TODO 3

--- 6-3-node-express/backend/utils/random.js
+++ 6-3-node-express/backend/utils/random.js
@@ -1,4 +1,5 @@
 // TODO 2: Create & Implement getRandomInt function
+import { getRandomInt } from "./utils/random.js";
 export function getRandomInt(max) {
   // Return a random integer from 0 up to max - 1
   return Math.floor(Math.random() * max);
